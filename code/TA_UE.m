7ad494c216c976434e6b4cdb91e571606f6787ed
Volodymyr Shevchenko <hzvfan@proton.me>
2025-12-05 13:28:37 +0000

    Revert "Refactor Frank Wolfe algorithm UE program"
    This reverts commit f0a219e38f27b0e1b10849f251ba204b576667df.

--- code/TA_UE.m
+++ code/TA_UE.m
@@ -1,120 +1,169 @@
 clc; clear; close all;
+tol = 58.6;
 
-distance_tolerance = 58.6;
+%% DEFINE CONSTANTS %%
+parentDir = ''; % INSERT MAIN DIRECTORY HERE
+inputDir = [parentDir 'data/inputs/'];
+outputDir = [parentDir 'data/outputs/'];
 
-data_root_dir   = 'C:/Users/elp24vs/Documents/Samantha Project/no2d/data/';
-input_dir_path  = [data_root_dir 'inputs/'];
-output_dir_path = [data_root_dir 'outputs/'];
+%% LOAD DATA %%
+edges = readtable([inputDir 'edges.csv']);
+nodes = readtable([inputDir 'nodes.csv']);
 
-%% LOAD NETWORK DATA
-edge_table = readtable([input_dir_path 'edges.csv']);
-node_table = readtable([input_dir_path 'nodes.csv']);
+highway = edges.highway;
+edges = readmatrix([inputDir 'edges.csv']);
 
-from_nodes  = edge_table{:, 1} + 1;
-to_nodes    = edge_table{:, 2} + 1;
-edge_weight = edge_table{:, 11};
+G = digraph(edges(:,1)+1, edges(:,2)+1, edges(:,11));
+G.Nodes.Lon = nodes.x; G.Nodes.Lat = nodes.y;
+G.Nodes.LSOA = nodes.LSOA;
+G.Edges.Speedlim = edges(:,17);
+G.Edges.Lanes = edges(:,18);
+G.Edges.Width = edges(:,19);
+G.Edges.Capacity = edges(:,23);
+G.Edges.CriticalDensity = edges(:,26);
+G.Edges.AvgSpeed = edges(:,25);
 
-road_network = digraph(from_nodes, to_nodes, edge_weight);
+lDists = readtable([inputDir ['lDists_tol' num2str(tol) '.csv']]);
 
-road_network.Nodes.Lon  = node_table.x;
-road_network.Nodes.Lat  = node_table.y;
-road_network.Nodes.LSOA = node_table.LSOA;
+OD_list = readmatrix([inputDir ['OD_list_tol' num2str(tol) '.csv']]);
+OD_list = OD_list(2:end,:);
+% PLUS ONE DUE TO PYTHON INDEXING IN ORIGINAL FILE
+OD_list(:,3) = OD_list(:,3) + 1;
+OD_list(:,4) = OD_list(:,4) + 1;
 
-road_network.Edges.Speedlim        = edge_table{:, 17};
-road_network.Edges.Lanes           = edge_table{:, 18};
-road_network.Edges.Width           = edge_table{:, 19};
-road_network.Edges.Capacity        = edge_table{:, 23};
-road_network.Edges.CriticalDensity = edge_table{:, 26};
-road_network.Edges.AvgSpeed        = edge_table{:, 25};
+OD_matrix = readmatrix([inputDir 'OD_matrix.csv']);
+OD_matrix = OD_matrix(2:end,:);
 
-OD_list = readmatrix([input_dir_path 'OD_list_tol' num2str(distance_tolerance) '.csv']);
-OD_list = OD_list(2:end, :);
+OD_names = readcell([inputDir 'OD_names.csv']);
+OD_names(1) = [];
 
-OD_list(:, 3:4) = OD_list(:, 3:4) + 1;
-
-demand = readmatrix([input_dir_path 'demand.csv']);
+demand = readmatrix([inputDir 'demand.csv']);
 demand = demand(2:end);
 
-%% REMOVE INTRA-LSOA DEMAND
-is_intra_lsoa = OD_list(:, 1) == OD_list(:, 2);
-OD_list(is_intra_lsoa, :) = [];
-demand(is_intra_lsoa)    = [];
+n = length(OD_names);
 
-capacity                = road_network.Edges.Capacity;
-critical_density        = road_network.Edges.CriticalDensity;
-road_length_km          = road_network.Edges.Weight ./ 1000;
-speedlimit_km_per_h     = road_network.Edges.Speedlim;
-free_flow_travel_time_h = road_length_km ./ speedlimit_km_per_h;
+numHours = 2+3; % how many hours of data is in this OD timeband
 
-number_of_graph_edges = numedges(road_network);
+%% REMOVE INTRA-LSOA DEMAND %%
+inds = find(OD_list(:,1)==OD_list(:,2));
 
-% BPR parameters (uniform)
-alpha = 0.15 * ones(number_of_graph_edges, 1);
-beta  = 4    * ones(number_of_graph_edges, 1);
+OD_list(inds,:) = [];
+demand(inds) = [];
 
-bpr_params = [alpha, beta, zeros(number_of_graph_edges, 1)]; % last col = epsilon (0 here)
+%% ASSIGN BPR FUNCTION PARAMETERS PER EDGE & OTHER MEASURES %%
+carsAndTaxis2019 = 0.78;
+carTaxiProportion = 0.95;
+demandTot=demand/(carsAndTaxis2019*carTaxiProportion);
+capacity = G.Edges.Capacity;
+criticalDensity = G.Edges.CriticalDensity;
+avgSpeed = G.Edges.AvgSpeed;
 
-eps       = 1e-5;
-steplimit = 200;
+% road length in meters to km
+distance = G.Edges.Weight ./ 1000;
+% speedlim in km/hour
+speed = G.Edges.Speedlim;
+% free-flow travel times in km/hour
+time = distance./speed;
 
-suffix = '_test';
+% **Literature-Based Approach**
+% alpha range: 0-1, generally 0.15
+% beta range: 2-5, generally 4
+% - Urban roads: alpha = 0.15, beta = 4.0
+% - Highway: alpha = 0.75, beta = 4.0
+% - Rural roads: alpha = 0.5, beta = 4.0
 
-txtName = [data_root_dir 'Out_TA_HPC_UE' suffix '.txt'];
-s = sprintf('File created: %s.\n', string(datetime('now')));
-writematrix(s, txtName);
-
-critLogName   = [data_root_dir 'All_crit1_crit2_UE' suffix '.csv'];
-critBestsName = [data_root_dir 'Best_crit1_crit2_UE' suffix '.csv'];
-
-writematrix(inf(steplimit + 1, 2), critLogName);
-writematrix([inf, inf, inf], critBestsName);
-
-%% TRAFFIC ASSIGNMENT MODEL
-tic
-
-TimeBinPeriods = "DAY";  % placeholder; length can be > 1 in future
-number_of_time_bins  = numel(TimeBinPeriods);
-
-UEflows     = zeros(number_of_graph_edges, number_of_time_bins);
-UEflowsBest = zeros(number_of_graph_edges, number_of_time_bins);
-
-for i = 1:number_of_time_bins
-    disp("Time bin ..." + string(i));
-    disp('Starting user-equilibrium Frank-Wolfe...');
-
-    [UEflows(:, i), ...
-        crit1_UE, crit2_UE, L_UE, ...
-        ~, critLog, critBests, ... %#ok<NASGU>
-        UEflowsBest(:, i), ...
-        crit1_UE_Best, crit2_UE_Best, ...
-        iter_UE, LBD_UE, LBD_UE_Best] = ...
-        FrankWolfe_UE_Flex( ...
-            demand, ...
-            free_flow_travel_time_h, ...
-            road_network, ...
-            OD_list, ...
-            eps, ...
-            capacity, ...
-            critical_density, ...
-            bpr_params, ...
-            steplimit, ...
-            txtName, ...
-            critLogName, ...
-            critBestsName);
-
+roadClass = cell(height(edges),1);
+for ii = 1:height(edges)
+    if contains(highway(ii),'motorway') | contains(highway(ii),'trunk')
+        roadClass(ii) = {'highway'};
+    else
+        if contains(highway(ii),'tertiary') | contains(highway(ii),'unclassified')
+            roadClass(ii) = {'rural'};
+        else
+            roadClass(ii) = {'urban'};
+        end
+    end
 end
 
+% for the BPR function
+alpha = repelem(0.15,height(G.Edges.EndNodes))';
+beta = repelem(4,height(G.Edges.EndNodes))';
+% alpha = zeros(height(edges),1);
+% beta = zeros(height(edges),1);
+% for ii = 1:height(edges)
+%     if contains(roadClass{ii},'highway')
+%         alpha(ii) = 0.75; beta(ii) = 4;
+%     else
+%         if contains(roadClass{ii},'rural')
+%             alpha(ii) = 0.5; beta(ii) = 4;
+%         else
+%             alpha(ii) = 0.15; beta(ii) = 4;
+%         end
+%     end
+% end
+params = [alpha, beta, repmat(0, [height(edges),1])]; % final value is convergence epsilon
+
+SuperNode = G.Nodes; % table of the nodes
+SuperEdge = G.Edges; % table of the edges
+
+elapsedTime=[];
+res_choice_count=1;
+TimeBinPeriods="DAY";%numHours;
+
+% FW Options
+eps=1e-5; %  PATIL ET AL. recommends 10^-5
+steplimit=125000;
+plotter=0;
+%
+UEflows=[];
+POflows=[];
+
+CFNE=[];
+CFSO=[];
+
+
+CFNE_edge=[];
+CFSO_edge=[];
+user_tt=[];
+system_tt=[];
+
+%% CREATE ITERATIVELY EDITED TXT & CSV OUTPUT FILES %%
+txtName = [parentDir 'Out_TA_HPC_UE.txt'];
+s = sprintf('File created: ' + string(datetime('now')) + '.\n');
+writematrix(s, txtName);
+
+critLogName = [parentDir 'All_crit1_crit2_UE.csv'];
+writematrix(inf(steplimit+1,2), critLogName);
+
+critBestsName = [parentDir 'Best_crit1_crit2_UE.csv'];
+writematrix([inf,inf,inf], critBestsName);
+
+%% BEGIN TRAFFIC ASSIGNMENT MODEL %%
+tic
+% topo_graph = G;
+% stepbreak = steplimit;
+for i=1:length(TimeBinPeriods)
+    disp(strcat('Time bin ...',string(i)))
+
+    disp('Starting user-equilibrium Frank-Wolfe...')
+    [UEflows(:,i),crit1_UE,crit2_UE,L_UE,~,critLog,critBests,UEflowsBest(:,i),crit1_UE_Best,crit2_UE_Best,iter_UE,LBD_UE,LBD_UE_Best]=FrankWolfe_UE_Flex(demand,time,G,OD_list,eps,capacity,criticalDensity,params,steplimit,txtName,critLogName,critBestsName);
+end
 toc
 
-%% WRITE RESULTS
-writematrix(UEflows,      [output_dir_path 'UE_flow'       suffix '.csv']);
-writematrix(UEflowsBest,  [output_dir_path 'UE_flow_best'  suffix '.csv']);
+%% WRITE RESULTS %%
+% flows
+writematrix(UEflows, [outputDir 'UE_flow.csv']);
+writematrix(UEflows, [outputDir 'UE_flow_best.csv']);
 
-writematrix([crit1_UE,     crit2_UE],     [output_dir_path 'UE_crit1and2'      suffix '.csv']);
-writematrix([crit1_UE_Best, crit2_UE_Best], [output_dir_path 'UE_crit1and2_best' suffix '.csv']);
+% crit values
+writematrix([crit1_UE, crit2_UE], [outputDir 'UE_crit1and2.csv']);
+writematrix([crit1_UE_Best, crit2_UE_Best], [outputDir 'UE_crit1and2_best.csv']);
 
-writematrix(L_UE,    [output_dir_path 'UE_L'      suffix '.csv']);
-writematrix(iter_UE, [output_dir_path 'UE_L_best' suffix '.csv']);
+% L (iteration at which the code reached optimum and/or timed out)
+writematrix(L_UE, [outputDir 'UE_L.csv']);
+writematrix(iter_UE, [outputDir 'UE_L_best.csv']);
 
-writematrix(LBD_UE,     [output_dir_path 'UE_LBD'      suffix '.csv']);
-writematrix(LBD_UE_Best,[output_dir_path 'UE_LBD_best' suffix '.csv']);
+% LBD
+writematrix(LBD_UE, [outputDir 'UE_LBD.csv']);
+writematrix(LBD_UE_Best, [outputDir 'UE_LBD_best.csv']);
+
