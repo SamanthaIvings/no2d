12e2a6341002b8ad48f0e2c5d5017c1048855c21
Volodymyr Shevchenko <hzvfan@proton.me>
2025-12-05 13:28:24 +0000

    Revert "Refactor Frank Wolfe module"
    This reverts commit 64965583084bd18aadaa99e5e73521040d9edc4c.

--- code/frank_wolfe/__init__.py
+++
@@ -1,4 +0,0 @@
-from code.frank_wolfe.settings import FrankWolfeSettings
-from code.frank_wolfe.solver import frank_wolfe_user_equilibrium
-
-__all__ = ["FrankWolfeSettings", "frank_wolfe_user_equilibrium"]
